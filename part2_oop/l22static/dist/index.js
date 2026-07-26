05f5c7e516cd244300308d273591ccb49d1a575c
6rose9 <zinzinthinzaw8@gmail.com>
2026-07-26 07:34:21 +0630

    static

--- part2_oop/l22static/dist/index.js
+++ part2_oop/l22static/dist/index.js
@@ -8,19 +8,49 @@ class People {
     getintro() {
         return `I\'m ${this.name}`;
     }
-    static over18(age) {
+    static isOver18(age) {
         return age > 18;
     }
 }
 let girl = new People("Crystal");
 console.log(girl); // People { name: 'Crystal' }
+console.log(People.isOver18(20));
 class Calculator {
+    static num = 0;
     static add(a, b) {
         return a + b;
     }
     static square(n) {
         return n * n;
     }
+    static increment() {
+        return ++Calculator.num;
+    }
+    static decrement() {
+        return --Calculator.num;
+    }
+    showNumber() {
+        console.log(`Current Number is ${Calculator.num}`);
+    }
 }
 console.log(Calculator.square(2));
 console.log(Calculator.add(5, 6));
+Calculator.increment();
+Calculator.increment();
+Calculator.decrement();
+let calculator = new Calculator();
+calculator.showNumber();
+class Staff {
+    name;
+    static total = 0;
+    constructor(name) {
+        this.name = name;
+        Staff.total++;
+    }
+    static totalStaff() {
+        console.log(`Total staff is ${Staff.total}`);
+    }
+}
+let staff1 = new Staff("Daw Su Su");
+let staff2 = new Staff("U Ba Aye");
+Staff.totalStaff();
--- part2_oop/l22static/src/index.ts
+++ part2_oop/l22static/src/index.ts
@@ -11,15 +11,18 @@ class People {
         return `I\'m ${this.name}`;
     }
 
-    static over18(age: number): boolean {
+    static isOver18(age: number): boolean {
         return age > 18;
     }
 }
 
 let girl = new People("Crystal");
 console.log(girl); // People { name: 'Crystal' }
+console.log(People.isOver18(20));
 
 class Calculator {
+
+    static num: number = 0;
 
     static add(a: number, b: number) {
         return a + b;
@@ -28,7 +31,43 @@ class Calculator {
     static square(n: number) {
         return n * n;
     }
+
+    static increment() {
+        return ++Calculator.num;
+    }
+
+    static decrement() {
+        return --Calculator.num;
+    }
+
+    showNumber() {
+        console.log(`Current Number is ${Calculator.num}`);
+    }
 }
 
 console.log(Calculator.square(2));
 console.log(Calculator.add(5, 6));
+
+Calculator.increment();
+Calculator.increment();
+Calculator.decrement();
+
+let calculator = new Calculator();
+calculator.showNumber();
+
+class Staff {
+    static total: number = 0;
+
+    constructor(public name: string) {
+        Staff.total++;
+    }
+
+    static totalStaff(): void {
+        console.log(`Total staff is ${Staff.total}`);
+    }
+}
+
+let staff1 = new Staff("Daw Su Su");
+let staff2 = new Staff("U Ba Aye");
+
+Staff.totalStaff();
